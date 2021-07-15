b2df997b6ab2cb8a393431670c7d7a029d3ad600
Do Hyeong Kim <dhk21512@gmail.com>
2021-07-15 14:18:04 +0900

    init

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Habit from "./components/habit";
+import "./app.css";
+
+function App() {
+  return (
+    <li className="habit">
+      <h1>Hello</h1>
+      <Habit />
+    </li>
+  );
+}
+
+export default App;
