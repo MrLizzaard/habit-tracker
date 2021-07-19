caa6809664d4eac5006e312541f2e13e7cf26b58
Do Hyeong Kim <dhk21512@gmail.com>
2021-07-19 14:36:07 +0900

    navbar

--- src/app.jsx
+++ src/app.jsx
@@ -4,16 +4,39 @@ import Navbar from "./components/navbar";
 import "./app.css";
 
 class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const habitCount = habits[index].count - 1;
+    habits[index].count = habitCount < 0 ? 0 : habitCount;
+    this.setState({ habits });
+  };
+
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
   render() {
     return (
       <>
-        <Navbar></Navbar>
-        <input className="input" type="text" placeholder="Habit" />
-        <button className="addBtn">Add</button>
-        <li className="habit">
-          <Habits />
-        </li>
-        <button className="resetBtn">Reset All</button>
+        <Navbar count={this.state.habits.filter((item) => item.count > 0).length}></Navbar>
+        <Habits habits={this.state.habits} onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} onDelete={this.handleDelete} />
       </>
     );
   }
